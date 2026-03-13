05b472b0f53c7dcb6ef5fdbdca241e8c444a6feb
zhrssh <98146850+zhrssh@users.noreply.github.com>
2026-03-13 10:34:20 -0400

    Added cORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -206,3 +206,9 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+    def test_cors(self):
+        """It should have CORS headers"""
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "*")
